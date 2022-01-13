d80d8941438167e2dbafbdecf21ea06c19d5a353
Florian Dauphin <florian.dauphin@objectiveplatform.com>
2022-01-13 15:31:37 +0100

    0.0.27 release hardcode intall requires in setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,6 @@ from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
 
-REQUIREMENTS = [
-    line.strip().split()[0]
-    for line in open(path.join(this_directory, "requirements.txt")).readlines()
-]
-
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
@@ -20,7 +15,7 @@ setup(
     ],
     description='TimeChimp API Python SDK',
     download_url='https://github.com/Afilnor/TimeChimp/archive/refs/heads/master.zip',
-    install_requires=[REQUIREMENTS],
+    install_requires=["build", "requests"],
     keywords=['timechimp'],
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -29,5 +24,5 @@ setup(
     platforms=['any'],
     python_requires='>=3.6',
     url='https://github.com/Afilnor/TimeChimp',
-    version="0.0.26",
+    version="0.0.27",
 )
