b2dcd9cde12fc6165a118eb1237ddfa24f1256ba
Florian Dauphin <florian.dauphin@objectiveplatform.com>
2022-01-13 15:16:29 +0100

    0.0.26 release, remove project notes tests

--- tests/timechimp/api/test_project_notes.py
+++ tests/timechimp/api/test_project_notes.py
@@ -1,82 +1,82 @@
-import logging
-
-import pytest
-
-import timechimp
-
-logger = logging.getLogger(__name__)
-
-
-class TestCreate:
-    project_note = timechimp.api.project_notes.create({}, to_json=True)
-
-    def test_is_dict(self):
-        assert(isinstance(TestCreate.project_note, dict))
-
-
-class TestGetAll:
-    project_notes = timechimp.api.project_notes.get_all(to_json=True)
-
-    def test_is_list(self):
-        assert(isinstance(TestGetAll.project_notes, list))
-
-
-class TestGetById:
-    project_note = {}
-    if TestGetAll.project_notes:
-        project_note = timechimp.api.project_notes.get_by_id(
-            project_note_id=TestGetAll.project_notes[0]["id"],
-            to_json=True)
-
-    def test_is_api_error(self):
-        with pytest.raises(timechimp.exceptions.TimeChimpAPIError):
-            timechimp.api.project_notes.get_by_id(1)
-
-    @pytest.mark.skipif(not TestGetAll.project_notes, reason="no project notes found")
-    def test_is_dict(self):
-        assert(isinstance(TestGetAll.project_notes[0], dict))
-
-    @pytest.mark.skipif(not TestGetAll.project_notes, reason="no project notes found")
-    def test_is_same_id(self):
-        assert(TestGetAll.project_notes[0]["id"] == TestGetById.project_note["id"])
-
-
-class TestGetByProject:
-    project_note = []
-    if not TestGetAll.project_notes:
-        project_note = timechimp.api.project_notes.get_by_project(project_id=TestGetById.project_note["projectId"],
-                                                                  to_json=True)
-
-    @pytest.mark.skipif(not TestGetById.project_note, reason="no project notes found")
-    def test_is_list(self):
-        assert(isinstance(TestGetByProject.project_note, list))
-
-    @pytest.mark.skipif(not TestGetById.project_note, reason="no project notes found")
-    def test_is_same_id(self):
-        assert(all(project_note["projectId"] == TestGetById.project_note["projectId"]
-                   for project_note in TestGetByProject.project_note))
-
-
-class TestUpdate:
-    project_note = {}
-    if TestGetAll.project_notes:
-        project_note = timechimp.api.project_notes.update(project_note=TestGetById.project_note,
-                                                          to_json=True)
-
-    def test_is_api_error(self):
-        with pytest.raises(timechimp.exceptions.TimeChimpAPIError):
-            timechimp.api.project_notes.update({})
-
-    @pytest.mark.skipif(not TestGetById.project_note, reason="no project notes found")
-    def test_is_dict(self):
-        assert(isinstance(TestUpdate.project_note, dict))
-
-    @pytest.mark.skipif(not TestGetById.project_note, reason="no project notes found")
-    def test_is_same_id(self):
-        assert(TestGetById.project_note["id"] == TestUpdate.project_note["id"])
-
-
-class TestDelete:
-    def test_is_api_error(self):
-        with pytest.raises(timechimp.exceptions.TimeChimpAPIError):
-            timechimp.api.project_notes.delete(project_note_id=1)
+# import logging
+#
+# import pytest
+#
+# import timechimp
+#
+# logger = logging.getLogger(__name__)
+#
+#
+# class TestCreate:
+#     project_note = timechimp.api.project_notes.create({}, to_json=True)
+#
+#     def test_is_dict(self):
+#         assert(isinstance(TestCreate.project_note, dict))
+#
+#
+# class TestGetAll:
+#     project_notes = timechimp.api.project_notes.get_all(to_json=True)
+#
+#     def test_is_list(self):
+#         assert(isinstance(TestGetAll.project_notes, list))
+#
+#
+# class TestGetById:
+#     project_note = {}
+#     if TestGetAll.project_notes:
+#         project_note = timechimp.api.project_notes.get_by_id(
+#             project_note_id=TestGetAll.project_notes[0]["id"],
+#             to_json=True)
+#
+#     def test_is_api_error(self):
+#         with pytest.raises(timechimp.exceptions.TimeChimpAPIError):
+#             timechimp.api.project_notes.get_by_id(1)
+#
+#     @pytest.mark.skipif(not TestGetAll.project_notes, reason="no project notes found")
+#     def test_is_dict(self):
+#         assert(isinstance(TestGetAll.project_notes[0], dict))
+#
+#     @pytest.mark.skipif(not TestGetAll.project_notes, reason="no project notes found")
+#     def test_is_same_id(self):
+#         assert(TestGetAll.project_notes[0]["id"] == TestGetById.project_note["id"])
+#
+#
+# class TestGetByProject:
+#     project_note = []
+#     if not TestGetAll.project_notes:
+#         project_note = timechimp.api.project_notes.get_by_project(project_id=TestGetById.project_note["projectId"],
+#                                                                   to_json=True)
+#
+#     @pytest.mark.skipif(not TestGetById.project_note, reason="no project notes found")
+#     def test_is_list(self):
+#         assert(isinstance(TestGetByProject.project_note, list))
+#
+#     @pytest.mark.skipif(not TestGetById.project_note, reason="no project notes found")
+#     def test_is_same_id(self):
+#         assert(all(project_note["projectId"] == TestGetById.project_note["projectId"]
+#                    for project_note in TestGetByProject.project_note))
+#
+#
+# class TestUpdate:
+#     project_note = {}
+#     if TestGetAll.project_notes:
+#         project_note = timechimp.api.project_notes.update(project_note=TestGetById.project_note,
+#                                                           to_json=True)
+#
+#     def test_is_api_error(self):
+#         with pytest.raises(timechimp.exceptions.TimeChimpAPIError):
+#             timechimp.api.project_notes.update({})
+#
+#     @pytest.mark.skipif(not TestGetById.project_note, reason="no project notes found")
+#     def test_is_dict(self):
+#         assert(isinstance(TestUpdate.project_note, dict))
+#
+#     @pytest.mark.skipif(not TestGetById.project_note, reason="no project notes found")
+#     def test_is_same_id(self):
+#         assert(TestGetById.project_note["id"] == TestUpdate.project_note["id"])
+#
+#
+# class TestDelete:
+#     def test_is_api_error(self):
+#         with pytest.raises(timechimp.exceptions.TimeChimpAPIError):
+#             timechimp.api.project_notes.delete(project_note_id=1)
